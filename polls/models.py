0845d677e5c340378cd370de82462014ef2d60ad
TonyH98 <tonyhoang@pursuit.org>
2025-03-12 11:12:27 -0400

    Add the users_games table

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
 class Users(models.Model):
@@ -18,4 +19,16 @@ class game_review(models.Model):
         MinValueValidator(0),
         MaxValueValidator(100)
     ])
+
+
+class users_games(models.Model):
+    game_title = models.CharField()
+    console = models.CharField()
+    release_date = models.DateField()
+    game_rating = models.CharField()
+    game_console = ArrayField(models.CharField())
+    game_dev = models.CharField()
+    game_publisher = models.CharField()
+    game_art = models.CharField()
+
 
